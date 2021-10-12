294c3a96c13381d286e6ca2104d50984bf21dfa5
Toufic Lawand <tlawand@users.noreply.github.com>
2021-10-12 11:46:35 -0400

    rem code snipet from main.py, add gtfs to reqs

--- main.py
+++ main.py
@@ -25,20 +25,3 @@ feed.ParseFromString(ace_resp.content)
 for entity in feed.entity:
     if entity.HasField("trip_update"):
         print(entity.trip_update)
-
-
-"""
-SAMPLE CODE FROM GOOGLE TRANSIT (https://developers.google.com/transit/gtfs-realtime/examples/python-sample)
-
-
-from google.transit import gtfs_realtime_pb2
-import urllib
-
-feed = gtfs_realtime_pb2.FeedMessage()
-response = urllib.request.urlopen('URL OF YOUR GTFS-REALTIME SOURCE GOES HERE')
-    # ^ this needs headers to work with MTA data, not sure how to do that
-feed.ParseFromString(response.read())
-for entity in feed.entity:
-  if entity.HasField('trip_update'):
-    print(entity.trip_update)
-"""
